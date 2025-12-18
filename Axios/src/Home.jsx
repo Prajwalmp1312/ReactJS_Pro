9d902fb2d605eb088affb0375b598447089a6701
Prajwalmp1312 <prajwalprabhakar1312@gmail.com>
2025-12-18 10:37:08 +0530

    Comment out image import in Home.jsx
    Commented out the import statement for Shopping1.jpg.

--- Axios/src/Home.jsx
+++ Axios/src/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link,Outlet} from 'react-router-dom'
 import "./App.css"
-import pic from "./Photos/Shopping1.jpg"
+//import pic from "./Photos/Shopping1.jpg"
 import Home1 from './Home1'
 
 const Home = () => {
